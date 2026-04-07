b5aa9f2823b7b0acb8b0c2c7e6fbdc087ebd903d
prafdin <pravdin.biss@gmail.com>
2026-04-08 00:11:33 +0400

    Revert "use cpp back"
    This reverts commit f2aa06500618862532fb2c910202c04132f27875.

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+import socket
+import datetime
+import requests
+
+app = Flask(__name__)
+HTTPBIN_DELAY_URL = "https://httpbin.org/delay/0.1"
+
+@app.route('/info')
+def get_info():
+    resp = requests.get(HTTPBIN_DELAY_URL)
+    return jsonify({
+        'hostname': socket.gethostname(),
+        'timestamp': datetime.datetime.now().isoformat(),
+        'message': 'Backend service is running!'
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
